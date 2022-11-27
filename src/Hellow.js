dc060dc621f8a36bffae0c16c642a4e1418a76e4
anishazahan <anishazahan13@gmail.com>
2022-11-27 16:52:26 +0600

    react app install

---
+++ src/Hellow.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import Heading from "./Heading";
+
+
+
+
+export default function(){
+
+    return(
+        <div>
+            <h1>Hellow world!</h1>
+        </div>
+    )
+
+}
+
