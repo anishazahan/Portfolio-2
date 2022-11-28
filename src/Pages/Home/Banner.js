63389a62a47b4570e60a6df49a3ce25c9a016691
anishazahan <anishazahan13@gmail.com>
2022-11-28 15:19:00 +0600

    react icon install

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -15,7 +15,7 @@ const Banner = () => {
                <p className='text-amber-50 font-medium my-5'>I am a Fronted developer.I will create Your website
                as your creative choice!</p>
         </div>
-        <div className="img-section w-[50%]">
+        <div className="img-section w-[50%] flex">
             <div className="h-full flex justify-center items-center relative">
                 <img className='w-[70%] h-[60%] object-cover absolute' src={banner} alt="" />
                 <div class=" w-[70%] h-[60%] relative  before:absolute before:-bottom-4 before:-right-4 before:h-28 before:w-28 before:border-b-4 before:border-r-4 before:border-emerald-600 before:transition-all before:duration-500 before:ease-in-out after:absolute after:-top-4 after:-left-4 after:h-28 after:w-28 after:border-t-4 after:border-l-4 after:border-emerald-600 after:transition-all after:duration-300 after:ease-in-out hover:before:h-[calc(100%+16px)] hover:before:w-[calc(100%+16px)] hover:after:h-[calc(100%+16px)] hover:after:w-[calc(100%+16px)]"></div>
