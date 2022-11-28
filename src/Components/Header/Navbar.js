a6ddb8631be075837a19190d549c08bcb6d6453c
anishazahan <anishazahan13@gmail.com>
2022-11-28 15:02:11 +0600

    banner img position fixed

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     const [nav, setNav] = useState(false);
   return (
     <>
-        <div className='bg-black py-2 '>
+        <div className='bg-[#1D1D1D] py-2 '>
                <nav className=" text-emerald-200 md:flex items-center  justify-between mx-auto max-w-7xl">
                     <div className="">
                         <img src={logo} alt="" />
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -4,30 +4,24 @@ import banner from '../../img/1_YZ2fsT9k1CmlMil-Fda0Zg.png'
 
 const Banner = () => {
   return (
-    <>
+    <div className='banner'>
 
-      <div className="flex h-[90vh]">
-        <div className="content w-[65%] bg-[#1D1D1D]">
+      <div className="flex h-[90vh] max-w-7xl">
+        <div className="content w-[50%]">
                 <h2>Lorem ipsum dolor sit amet.</h2>
         </div>
-        <div className="img-section w-[35%] bg-black">
-            <div className="h-full flex justify-start ml-[-13rem] items-center relative">
-                <img className='w-[60%] h-[60%] object-cover absolute' src={banner} alt="" />
-                <div class=" w-[60%] h-[60%] relative  before:absolute before:-bottom-4 before:-right-4 before:h-28 before:w-28 before:border-b-4 before:border-r-4 before:border-emerald-600 before:transition-all before:duration-500 before:ease-in-out after:absolute after:-top-4 after:-left-4 after:h-28 after:w-28 after:border-t-4 after:border-l-4 after:border-emerald-600 after:transition-all after:duration-300 after:ease-in-out hover:before:h-[calc(100%+16px)] hover:before:w-[calc(100%+16px)] hover:after:h-[calc(100%+16px)] hover:after:w-[calc(100%+16px)]"></div>
+        <div className="img-section w-[50%]">
+            <div className="h-full flex justify-center items-center relative">
+                <img className='w-[70%] h-[60%] object-cover absolute' src={banner} alt="" />
+                <div class=" w-[70%] h-[60%] relative  before:absolute before:-bottom-4 before:-right-4 before:h-28 before:w-28 before:border-b-4 before:border-r-4 before:border-emerald-600 before:transition-all before:duration-500 before:ease-in-out after:absolute after:-top-4 after:-left-4 after:h-28 after:w-28 after:border-t-4 after:border-l-4 after:border-emerald-600 after:transition-all after:duration-300 after:ease-in-out hover:before:h-[calc(100%+16px)] hover:before:w-[calc(100%+16px)] hover:after:h-[calc(100%+16px)] hover:after:w-[calc(100%+16px)]"></div>
             </div>
        
         </div>
       </div>
 
 
-      {/* <!-- component --> */}
-{/* <div class="flex h-screen w-full items-center justify-center bg-slate-400"> */}
-  {/* <!-- Border animation Here --> */}
-  {/* <div class="relative h-20 w-20 border before:absolute before:-bottom-2 before:-right-2 before:h-4 before:w-4 before:border-b before:border-r before:transition-all before:duration-300 before:ease-in-out after:absolute after:-top-2 after:-left-2 after:h-4 after:w-4 after:border-t after:border-l after:transition-all after:duration-300 after:ease-in-out hover:before:h-[calc(100%+16px)] hover:before:w-[calc(100%+16px)] hover:after:h-[calc(100%+16px)] hover:after:w-[calc(100%+16px)]"></div>
-</div> */}
 
-
-    </>
+    </div>
   )
 }
 
