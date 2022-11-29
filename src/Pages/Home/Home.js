7f1a7dd6c76e400feba2751aaf2c77268327e00f
anishazahan <anishazahan13@gmail.com>
2022-11-29 15:29:20 +0600

    skills add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from '../About/About'
+import Skills from '../Projects/Skills'
 import Banner from './Banner'
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
    <>
     <Banner></Banner>
     <About></About>
+    <Skills></Skills>
 
  </>
   )
---
+++ src/Pages/Projects/Skills.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Skills = () => {
+
+    // const expert = [  Html, CSS, SASS, Bootstrap, Tailwind, DaysiUI, Javascript(es6), React.Js, React-Router, React-
+    //     Redux, React-Typescript.]
+  return (
+    <div className='bg-[#2e3434] py-10'>
+        <h2 className='text-3xl font-bold text-center text-gray-200 my-5'>My Technical Skills</h2>
+
+ <div className="mx-auto text-center max-w-7xl px-32">
+    <h2 className='text-2xl px-5 font-semibold text-gray-200 border-b-2 border-emerald-500 inline-block'>Expertice</h2>
+    <div className="flex my-16">
+
+        <div className="w-28 h-28 bg-black p-4 rounded-sm shadow-lg">
+            <img className='w-full h-full object-cover' src={'https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5.png'} alt="" />
+        </div>
+
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Skills
